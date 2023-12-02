5776893546b99883c2616f951eb3ee175b2b4d8a
Reaper Gelera <ahoy@barelyhuman.dev>
2023-12-02 12:38:11 +0530

    initial impl

---
+++ build.js
@@ -0,0 +1,96 @@
+import { writeFile } from "fs/promises";
+import { createContext } from "./src/index.ts";
+import tsc from "tsc-prog";
+
+const isDev = process.argv.includes("--dev");
+
+const buildContext = createContext({
+  bundle: true,
+});
+
+const common = {
+  external: ["tiny-glob", "defu", "esbuild"],
+  entryPoints: ["./src/index.js"],
+  platform: "node",
+  target: "node14",
+  format: "esm",
+};
+
+buildContext.config({
+  ...common,
+  outdir: "./dist/esm",
+  format: "esm",
+  outExtension: {
+    ".js": ".mjs",
+  },
+});
+
+buildContext.config({
+  ...common,
+  outdir: "./dist/cjs",
+  format: "cjs",
+  outExtension: {
+    ".js": ".js",
+  },
+});
+
+buildContext.on("error", (errors) => {
+  errors.map((x) => process.stdout.write(x.reason.toString() + "\n"));
+});
+
+buildContext.on("build", async () => {
+  process.stdout.write("[custom-builder] Built\n");
+  await writeFile(
+    "./dist/cjs/package.json",
+    JSON.stringify({ type: "commonjs" }),
+    "utf8"
+  );
+
+  await writeFile(
+    "./dist/esm/package.json",
+    JSON.stringify({ type: "module" }),
+    "utf8"
+  );
+
+  tsc.build({
+    basePath: process.cwd(),
+    compilerOptions: {
+      rootDir: "src",
+      outDir: "./dist/types",
+      declaration: true,
+      moduleResolution: "nodenext",
+      target: "esnext",
+      module: "NodeNext",
+      emitDeclarationOnly: true,
+      skipLibCheck: true,
+    },
+    include: ["src/**/*"],
+    exclude: ["**/*.test.ts", "**/*.spec.ts"],
+  });
+
+  if (isDev) return;
+
+  process.exit(0);
+});
+
+function createChain() {
+  let agg = Promise.resolve();
+  const _chainer = (fn) => {
+    agg = agg.then(fn);
+  };
+  _chainer.value = async () => {
+    await agg;
+    return null;
+  };
+  return _chainer;
+}
+
+const chain = createChain();
+
+if (isDev) {
+  chain(() => buildContext.watch());
+}
+
+chain(() => buildContext.build());
+
+await chain.value;
---
+++ src/index.ts
@@ -0,0 +1,78 @@
+import esbuild from "esbuild";
+import { defu } from "defu";
+import glob from "tiny-glob";
+import { EventEmitter } from "node:events";
+
+class ContextManager {
+  initialConfig: esbuild.BuildOptions = {};
+  contextConfigs: esbuild.BuildOptions[] = [];
+  contexts: esbuild.BuildContext[] = [];
+  queue = [];
+  #eventBus = new EventEmitter();
+
+  constructor(initial: esbuild.BuildOptions) {
+    this.initialConfig = initial;
+  }
+
+  config(conf: esbuild.BuildOptions) {
+    this.contextConfigs.push(defu(conf, this.initialConfig));
+  }
+
+  glob(pattern: string, opts: any) {
+    return glob(pattern, opts);
+  }
+
+  on(eventName: "error" | "build", listener: (...args: any[]) => void) {
+    this.#eventBus.addListener(eventName, listener);
+    return () => this.#eventBus.removeListener(eventName, listener);
+  }
+
+  offAll(eventName: string) {
+    this.#eventBus.removeAllListeners(eventName);
+  }
+
+  async #createContext() {
+    const eBus = this.#eventBus;
+    let cfg;
+
+    while ((cfg = this.contextConfigs.shift())) {
+      try {
+        cfg.plugins ||= [];
+
+        cfg.plugins.push({
+          name: "esbuild-multicontext-handler",
+          setup(build) {
+            build.onEnd((result) => {
+              eBus.emit(`built-context`, {
+                result,
+              });
+            });
+          },
+        });
+
+        const context = await esbuild.context(defu(cfg, this.initialConfig));
+
+        this.contexts.push(context);
+      } catch (err) {
+        this.#eventBus.emit("error", err);
+        break;
+      }
+    }
+  }
+
+  async build() {
+    await this.#createContext();
+    this.contexts.forEach((x) => x.rebuild());
+
+    this.#eventBus.emit("build");
+  }
+
+  async watch() {
+    await this.#createContext();
+    await Promise.all(this.contexts.map((x) => x.watch()));
+  }
+}
+
+export function createContext(initial: esbuild.BuildOptions) {
+  return new ContextManager(initial);
+}
